bac4b0df6f95192017ffd425a2efefdc93d08f8f
CassieCheng <swwstar@gmail.com>
2014-12-01 14:44:54 -0500

    New Controller

---
+++ Msi-Project/src/com/mercury/demand/service/CreditcardService.java
@@ -0,0 +1,32 @@
+package com.mercury.demand.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mercury.common.db.*;
+import com.mercury.demand.persistence.model.Creditcard;
+import com.mercury.demand.persistence.model.Trader;
+
+@Service
+@Transactional
+public class CreditcardService {
+
+	@Autowired
+	@Qualifier("creditcardDao")
+    Dao<Creditcard, Integer>creditcardDao;	
+	@Autowired
+	@Qualifier("traderDao")
+	Dao<Trader, Integer> traderDao;
+	
+	public void creditcard(Creditcard creditcard, Trader trader){
+		double balance=0;
+		//creditcardDao.save(creditcard);
+		trader.setBalance( balance);
+		trader.addCreditcard(creditcard);
+		traderDao.save(trader);
+		
+	}
+}
--- Msi-Project/src/com/mercury/demand/web/controller/AppController.java
+++ Msi-Project/src/com/mercury/demand/web/controller/AppController.java
@@ -1,13 +1,47 @@
 package com.mercury.demand.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mercury.demand.persistence.model.Creditcard;
+import com.mercury.demand.service.CreditcardService;
 
 @Controller
 @RequestMapping("/app")
 public class AppController {
 
+	@Autowired
+	private CreditcardService cs;
+	
+	public CreditcardService getCs() {
+		return cs;
+	}
+
+	public void setCs(CreditcardService cs) {
+		this.cs = cs;
+	}
+	
+	
+	public void enroll(HttpServletRequest request){
+		
+		
+		String card_holder = request.getParameter("Card_holder");
+		String card_number = request.getParameter("Card_number");
+		int expire_month = Integer.parseInt(request.getParameter("Expire_month"));
+		int expire_year = Integer.parseInt(request.getParameter("Expire_year"));
+		int code = Integer.parseInt(request.getParameter("Code"));
+		Creditcard creditcard = new Creditcard(card_holder,card_number,expire_month,expire_year,code);
+		
+		
+		//return null;
+	}
+	
+	
+	
 	@RequestMapping("/config.htm")
 	public ModelAndView config() {
 		return null;
